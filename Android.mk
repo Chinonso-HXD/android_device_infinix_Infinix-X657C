dfff45e6d4581948bb120f7b017117a2375b9111
Chinonso-HXD <149972125+Chinonso-HXD@users.noreply.github.com>
2023-11-08 02:17:02 +0100

    Android.mk

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := LOCAL_PATH := device/infinix/X657C
+LOCAL_PATH := device/infinix/X657C
 
 ifeq ($(TARGET_DEVICE),X657C)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
